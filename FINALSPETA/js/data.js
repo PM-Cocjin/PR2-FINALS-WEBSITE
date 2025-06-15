62676420b0af349ed7bbc7ec0697f75428e4153e
PM-Cocjin <paulcocjin3@gmail.com>
2025-06-15 22:55:05 +0800

    Add files via upload

---
+++ FINALSPETA/js/data.js
@@ -0,0 +1,14 @@
+const products = [
+    { id: 'p1', name: 'Graphic T-Shirt', price: 25, image: '../MIDTERM-PETA/images/product images/tshirt.png' },
+    { id: 'p2', name: 'Stylish Hoodie', price: 45, image: '../MIDTERM-PETA/images/product images/hoodie1.png' },
+    { id: 'p3', name: 'Modern Jacket', price: 60, image: '../MIDTERM-PETA/images/product images/jacket1.png' },
+    { id: 'p5', name: 'Casual Sneakers', price: 50, image: '../MIDTERM-PETA/images/product images/shoes.AVIF' },
+    { id: 'p4', name: 'Classic Cap', price: 20, image: '../MIDTERM-PETA/images/product images/cap1.png' },
+    { id: 'p6', name: 'Casual Tee', price: 30, image: '../MIDTERM-PETA/images/product images/tshirt1.png' },
+    { id: 'section80', name: 'Section 80 Vinyl', price: 120, image: '../MIDTERM-PETA/images1/section.80vinyl.png' },
+    { id: 'gkmc', name: 'GKMC Vinyl', price: 120, image: '../MIDTERM-PETA/images1/gkmcvinyl.png' },
+    { id: 'tpab', name: 'To Pimp A Butterfly Vinyl', price: 120, image: '../MIDTERM-PETA/images1/tpabvinyl.png' },
+    { id: 'damn', name: 'DAMN. Vinyl', price: 120, image: '../MIDTERM-PETA/images1/damnvinyl.png' },
+    { id: 'mmatbs', name: 'Mr. Morale and The Big Steppers Vinyl', price: 120, image: '../MIDTERM-PETA/images1/mmatbsvinyl.png' },
+    { id: 'gnx', name: 'GNX Vinyl', price: 120, image: '../MIDTERM-PETA/images1/gnxvinyl.png' },    
+  ];
